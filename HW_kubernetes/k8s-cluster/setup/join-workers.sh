f1538c6e1d0c82b0959375be6ce6dbec81808e8e
Dima <fantom2820281@gmail.com>
2025-11-24 04:11:36 +0000

    manifest

--- HW_kubernetes/k8s-cluster/setup/join-workers.sh
+++ HW_kubernetes/k8s-cluster/setup/join-workers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Пример (подставь реальную команду из вывода init-master.sh)
-sudo kubeadm join 192.168.1.10:6443 --token abcdef.1234567890abcdef \
-    --discovery-token-ca-cert-hash sha256:abcdef123456...
+kubeadm join 10.129.0.37:6443 --token v17bcz.f3tdinxe9bpvveck --discovery-token-ca-cert-hash sha256:7508d305f602d9ed814275a5bb5757dabc3027f930ad1c633f4d182164cc31bc
 
 # данные вносим получив их из результатов выполнения init-master.sh 
